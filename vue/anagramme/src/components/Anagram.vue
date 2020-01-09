ba318ec8d50ed5630b06d2c2b7c395f3f41c13ae
clement_besnier <clemsciences@aol.com>
2020-01-09 18:38:09 +0100

    Added skeleton of view

---
+++ vue/anagramme/src/components/Anagram.vue
@@ -0,0 +1,45 @@
+<template>
+    <v-container class="mx-6">
+        <h1>Anagram solver</h1>
+        <v-row>
+            <v-textarea solo name="textToDecipher" hint="Text to decipher"></v-textarea>
+        </v-row>
+        <v-row>
+            <v-btn @click="tokenize">Tokenize</v-btn>
+        </v-row>
+        <v-row>
+            <v-col>
+                <v-checkbox v-model="loadFromCLTK" label="Load from CLTK"></v-checkbox>
+            </v-col>
+            <v-col>
+                <v-checkbox v-model="loadFromUserInput" label="Load from your tokens"></v-checkbox>
+            </v-col>
+        </v-row>
+        <v-row>
+        <v-textarea solo name="lexicon" hint="Known tokens"></v-textarea>
+        </v-row>
+        <v-text></v-text>
+        
+    </v-container>
+
+</template>
+<script>
+export default {
+    data: () => {
+        return {
+            loadFromCLTK: false,
+            loadFromUserInput: false
+        }
+    },
+    methods: {
+        tokenize: function() {
+
+        },
+    },
+
+}
+</script>
+
+<style scoped>
+
+</style>
